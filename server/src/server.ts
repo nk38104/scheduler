7011204795fca76fcf7c8f751089284b1c16caf8
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-05 12:37:51 +0200

    Separate port into variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response, Application } from "express";
 
 const app: Application = express();
+const port = 3000;
 
 app.get("/", (req: Request, res: Response): void => {
   res.send("Hello there!");
 });
 
-app.listen(3000, (): void => {
-  console.log(`Server running on port 3000...`);
+app.listen(port || 5000, (): void => {
+  console.log(`Server running on port ${port}...`);
 });
