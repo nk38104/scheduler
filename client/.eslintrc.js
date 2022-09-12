bca102f73b235a8b743240e6f39b35bd23d76317
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-12 14:00:52 +0200

    Add eslint config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -4,18 +4,20 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
+    "@extensionengine",
     "eslint:recommended",
     "plugin:vue/vue3-recommended",
-    "@vue/eslint-config-prettier",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: [
-    "vue",
-  ],
+  plugins: ["vue", "prettier"],
   rules: {
+    "prettier/prettier": ["error"],
+    "vue/no-multiple-template-root": "off",
+    "no-unexpected-multiline": "error",
     "vue/no-unused-vars": "error",
     "vue/no-unused-properties": "error",
     "vue/no-undef-components": "error",
@@ -23,20 +25,25 @@ module.exports = {
     "vue/no-empty-component-block": "error",
     "vue/component-api-style": "warn",
     "vue/padding-line-between-blocks": "warn",
+    "vue/no-spaces-around-equal-signs-in-attribute": ["error"],
     "vue/component-name-in-template-casing": [
       "error",
       "PascalCase",
       {
-        "registeredComponentsOnly": true,
+        registeredComponentsOnly: true,
       },
     ],
     "vue/define-macros-order": [
       "error",
       {
-        "order": [
-          "defineProps",
-          "defineEmits",
-        ],
+        order: ["defineProps", "defineEmits"],
+      },
+    ],
+    "vue/max-attributes-per-line": [
+      "error",
+      {
+        singleline: 5,
+        multiline: 1,
       },
     ],
   },
