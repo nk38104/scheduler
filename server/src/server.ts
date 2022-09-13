52817a5526ab3adf99b3daddaa335ec9b12ccb53
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-13 13:26:56 +0200

    Remove server config variable import and replace its use

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,8 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { serverConfig } from "./config";
 
-const port = serverConfig.port;
-const hostname = serverConfig.hostname;
+const port = process.env.PORT;
+const hostname = process.env.HOST;
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
