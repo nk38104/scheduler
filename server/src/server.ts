a6e4b660805c00a1d6812f51016e0cc096b8774f
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-13 10:48:03 +0200

    Add init env variables to server.js

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,19 @@
+import * as dotenv from "dotenv";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 
+dotenv.config();
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
-const port = 3000;
+
+const hostname = process.env.HOST;
+const port = process.env.PORT;
 
 app.get("/", (_req: Request, res: Response): void => {
   res.send("Hello there!");
 });
 
 app.listen(port || 5000, (): void => {
-  console.log(`Server running on port ${port}...`);
+  console.log(`Server running at http://${hostname}:${port} ...`);
 });
