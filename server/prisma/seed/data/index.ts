ed37e05a0533da39cc7ee3f05d63d35e3c2307d7
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-27 21:26:50 +0200

    Add seed exporting from index file

---
+++ server/prisma/seed/data/index.ts
@@ -0,0 +1,8 @@
+import { classes } from "./classes";
+import { classrooms } from "./classrooms";
+import { subjects } from "./subjects";
+import { teachers } from "./teachers";
+import { timetables } from "./timetables";
+import { user } from "./user";
+
+export { classes, classrooms, teachers, timetables, user, subjects };
