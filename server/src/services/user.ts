6241fbd46c8d951d4240b58f207a8a59be995ce4
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-28 21:04:11 +0200

    Add update user service function

--- server/src/services/user.ts
+++ server/src/services/user.ts
@@ -1,15 +1,25 @@
 import { prisma } from "../utils/prisma.db";
 
-const get = async (userId: number) => {
-  const user = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
+const get = async (id: number) => {
+  const user = await prisma.user.findUniqueOrThrow({
+    where: { id },
   });
 
   return user;
 };
 
+const update = async (id: number, email: string) => {
+  const updateUser = await prisma.user.update({
+    where: { id },
+    data: {
+      email,
+    },
+  });
+
+  return updateUser;
+};
+
 export default {
   get,
+  update,
 };
