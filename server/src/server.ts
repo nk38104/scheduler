cad42939d6dc5060224130f9d8658a5f4bb3430e
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-28 20:39:26 +0200

    Implement router in server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,19 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import cors from "cors";
+import router from "./routes";
 import server from "./config/server";
 
 const port = server.port;
 const hostname = server.hostname;
 const app: Application = express();
 app.use(express.json());
-app.use(cors());
-
-app.get("/", (_req: Request, res: Response): void => {
-  res.send("Hello there!");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: true,
+  }),
+);
+app.use("/api", router);
 
 app.listen(port, (): void => {
   console.log(`Server running at 'http://${hostname}:${port}' ...`);
