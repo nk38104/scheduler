c3ebc3cbca1f144e94687450d028f4e7af52adbf
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-28 23:58:23 +0200

    Add crud subject controller

---
+++ server/src/controllers/subject.ts
@@ -0,0 +1,82 @@
+import { Request, Response } from "express";
+import subjectService from "../services/subject";
+
+const create = async (req: Request, resp: Response) => {
+  try {
+    const { userId, subjectCode, name, hoursPerWeek, maxHoursPerDay } = req.body;
+
+    const subject = await subjectService.create(
+      userId,
+      subjectCode,
+      name,
+      hoursPerWeek,
+      maxHoursPerDay,
+    );
+
+    resp.status(200).json(subject);
+  } catch (err: any) {
+    resp.status(500).send(err);
+  }
+};
+
+const get = async (req: Request, resp: Response) => {
+  try {
+    const id: number = req.body.id;
+
+    const subject = await subjectService.getById(id);
+
+    resp.status(200).json(subject);
+  } catch (err: any) {
+    resp.status(500).send(err.name);
+  }
+};
+
+const getAll = async (req: Request, resp: Response) => {
+  try {
+    const userId: number = req.body.id;
+
+    const subjects = await subjectService.getAll(userId);
+
+    resp.status(200).json(subjects);
+  } catch (err: any) {
+    resp.status(500).send(err.name);
+  }
+};
+
+const update = async (req: Request, resp: Response) => {
+  try {
+    const { id, subjectCode, name, hoursPerWeek, maxHoursPerDay } = req.body;
+
+    const subject = await subjectService.update(
+      id,
+      subjectCode,
+      name,
+      hoursPerWeek,
+      maxHoursPerDay,
+    );
+
+    resp.status(200).send(subject);
+  } catch (err: any) {
+    resp.status(500).send(err.meta.cause);
+  }
+};
+
+const remove = async (req: Request, resp: Response) => {
+  try {
+    const id: number = req.body.id;
+
+    const subject = await subjectService.remove(id);
+
+    resp.status(200).send(subject);
+  } catch (err: any) {
+    resp.status(500).send(err.meta.cause);
+  }
+};
+
+export default {
+  create,
+  get,
+  getAll,
+  update,
+  remove,
+};
