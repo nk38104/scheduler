8a720a5100d3382d298d6267b2242ea9a00d4923
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-29 00:12:40 +0200

    Add crud teacher controller

---
+++ server/src/controllers/teacher.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from "express";
+import teacherService from "../services/teacher";
+
+const create = async (req: Request, resp: Response) => {
+  try {
+    const { userId, teacherCode, firstName, lastName } = req.body;
+
+    const teacher = await teacherService.create(userId, teacherCode, firstName, lastName);
+
+    resp.status(200).json(teacher);
+  } catch (err: any) {
+    resp.status(500).send(err);
+  }
+};
+
+const get = async (req: Request, resp: Response) => {
+  try {
+    const id: number = req.body.id;
+
+    const teacher = await teacherService.getById(id);
+
+    resp.status(200).json(teacher);
+  } catch (err: any) {
+    resp.status(500).send(err.name);
+  }
+};
+
+const getAll = async (req: Request, resp: Response) => {
+  try {
+    const userId: number = req.body.id;
+
+    const teachers = await teacherService.getAll(userId);
+
+    resp.status(200).json(teachers);
+  } catch (err: any) {
+    resp.status(500).send(err.name);
+  }
+};
+
+const update = async (req: Request, resp: Response) => {
+  try {
+    const { id, teacherCode, firstName, lastName } = req.body;
+
+    const teacher = await teacherService.update(id, teacherCode, firstName, lastName);
+
+    resp.status(200).send(teacher);
+  } catch (err: any) {
+    resp.status(500).send(err.meta.cause);
+  }
+};
+
+const remove = async (req: Request, resp: Response) => {
+  try {
+    const id: number = req.body.id;
+
+    const teacher = await teacherService.remove(id);
+
+    resp.status(200).send(teacher);
+  } catch (err: any) {
+    resp.status(500).send(err.meta.cause);
+  }
+};
+
+export default {
+  create,
+  get,
+  getAll,
+  update,
+  remove,
+};
