739235313f3e8e8219d1030f3a3d1227e7e506ea
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-28 22:47:10 +0200

    Add timetable read route

--- server/src/routes/timetable.ts
+++ server/src/routes/timetable.ts
@@ -4,5 +4,6 @@ import timetableController from "../controllers/timetable";
 const router = Router();
 
 router.post("/", (req, resp) => timetableController.create(req, resp));
+router.get("/", (req, resp) => timetableController.get(req, resp));
 
 export default router;
