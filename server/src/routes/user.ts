ff789798ac59dbccece713034c3166fafacf72b3
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-28 21:04:30 +0200

    Add update controller function

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -4,5 +4,6 @@ import userController from "../controllers/user";
 const router = Router();
 
 router.get("/", (req, resp) => userController.get(req, resp));
+router.put("/", (req, resp) => userController.update(req, resp));
 
 export default router;
