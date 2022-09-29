b653dc5882d5f9f40c898090d109006403c0d5cd
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-29 11:04:49 +0200

    Add id as param to routes

--- server/src/routes/class.ts
+++ server/src/routes/class.ts
@@ -4,9 +4,9 @@ import { Router } from "express";
 const router = Router();
 
 router.post("/", (req, resp) => classController.create(req, resp));
-router.get("/", (req, resp) => classController.get(req, resp));
-router.get("/list", (req, resp) => classController.getAll(req, resp));
-router.put("/", (req, resp) => classController.update(req, resp));
-router.delete("/", (req, resp) => classController.remove(req, resp));
+router.get("/:id", (req, resp) => classController.get(req, resp));
+router.get("/", (req, resp) => classController.getAll(req, resp));
+router.put("/:id", (req, resp) => classController.update(req, resp));
+router.delete("/:id", (req, resp) => classController.remove(req, resp));
 
 export default router;
