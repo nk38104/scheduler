741470203ee378f75925cdceb9a967c515e7a2c1
Nikola Kelava <nikola.kelava94@gmail.com>
2022-09-29 10:57:43 +0200

    Add id as param to routes

--- server/src/routes/teacher.ts
+++ server/src/routes/teacher.ts
@@ -4,9 +4,9 @@ import teacherController from "../controllers/teacher";
 const router = Router();
 
 router.post("/", (req, resp) => teacherController.create(req, resp));
-router.get("/", (req, resp) => teacherController.get(req, resp));
-router.get("/list", (req, resp) => teacherController.getAll(req, resp));
-router.put("/", (req, resp) => teacherController.update(req, resp));
-router.delete("/", (req, resp) => teacherController.remove(req, resp));
+router.get("/:id", (req, resp) => teacherController.get(req, resp));
+router.get("/", (req, resp) => teacherController.getAll(req, resp));
+router.put("/:id", (req, resp) => teacherController.update(req, resp));
+router.delete("/:id", (req, resp) => teacherController.remove(req, resp));
 
 export default router;
